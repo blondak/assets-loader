d70450d83216a4cfcff58c1ad3c646e7e498f4b6
Zdeněk Jurka <zdenek.jurka@jware.cz>
2019-12-12 11:47:48 +0100

    Fixed strict_types errors

--- src/IPub/AssetsLoader/Components/CssLoader.php
+++ src/IPub/AssetsLoader/Components/CssLoader.php
@@ -25,180 +25,180 @@ use IPub\AssetsLoader\Files;
 
 class CssLoader extends AssetsLoader
 {
-	/**
-	 * @var string
-	 */
-	protected $title;
+    /**
+     * @var string
+     */
+    protected $title;
 
-	/**
-	 * @var string
-	 */
-	protected $contentType = 'text/css';
+    /**
+     * @var string
+     */
+    protected $contentType = 'text/css';
 
-	/**
-	 * @var bool
-	 */
-	protected $alternate = FALSE;
+    /**
+     * @var bool
+     */
+    protected $alternate = false;
 
-	/**
-	 * Set title
-	 *
-	 * @param string $title
-	 *
-	 * @return void
-	 */
-	public function setTitle(string $title) : void
-	{
-		$this->title = $title;
-	}
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return void
+     */
+    public function setTitle(string $title) : void
+    {
+        $this->title = $title;
+    }
 
-	/**
-	 * Get title
-	 *
-	 * @return string
-	 */
-	public function getTitle() : string
-	{
-		return $this->title;
-	}
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle() : string
+    {
+        return $this->title;
+    }
 
-	/**
-	 * Set alternate
-	 *
-	 * @param bool $alternate
-	 *
-	 * @return void
-	 */
-	public function setAlternate($alternate) : void
-	{
-		$this->alternate = $alternate;
-	}
+    /**
+     * Set alternate
+     *
+     * @param bool $alternate
+     *
+     * @return void
+     */
+    public function setAlternate($alternate) : void
+    {
+        $this->alternate = $alternate;
+    }
 
-	/**
-	 * Is alternate ?
-	 *
-	 * @return bool
-	 */
-	public function isAlternate() : bool
-	{
-		return $this->alternate;
-	}
+    /**
+     * Is alternate ?
+     *
+     * @return bool
+     */
+    public function isAlternate() : bool
+    {
+        return $this->alternate;
+    }
 
-	/**
-	 * Get link element
-	 *
-	 * @param string $source
-	 * @param string|NULL $media
-	 *
-	 * @return Utils\Html
-	 */
-	public function getElement(string $source, ?string $media = NULL) : Utils\Html
-	{
-		return Utils\Html::el('link')
-			->appendAttribute('rel', 'stylesheet' . ($this->isAlternate() ? ' alternate' : ''))
-			->appendAttribute('type', $this->contentType)
-			->appendAttribute('title', $this->title)
-			->appendAttribute('media', !trim($media) ? 'all' : $media)
-			->href($source);
-	}
+    /**
+     * Get link element
+     *
+     * @param string $source
+     * @param string|NULL $media
+     *
+     * @return Utils\Html
+     */
+    public function getElement(string $source, ?string $media = null) : Utils\Html
+    {
+        return Utils\Html::el('link')
+            ->appendAttribute('rel', 'stylesheet' . ($this->isAlternate() ? ' alternate' : ''))
+            ->appendAttribute('type', $this->contentType)
+            ->appendAttribute('title', $this->title)
+            ->appendAttribute('media', !trim($media?:'') ? 'all' : $media)
+            ->href($source);
+    }
 
-	/**
-	 * @return void
-	 *
-	 * @throws Nette\Application\UI\InvalidLinkException
-	 */
-	public function renderFiles() : void
-	{
-		// Remote files
-		foreach ($this->files->getRemoteFiles() as $file) {
-			echo $this->getElement($file), PHP_EOL;
-		}
+    /**
+     * @return void
+     *
+     * @throws Nette\Application\UI\InvalidLinkException
+     */
+    public function renderFiles() : void
+    {
+        // Remote files
+        foreach ($this->files->getRemoteFiles() as $file) {
+            echo $this->getElement($file), PHP_EOL;
+        }
 
-		$filesByMedia = [];
+        $filesByMedia = [];
 
-		/** @var Entities\IFile $file */
-		foreach ($this->files as $file) {
-			$filesByMedia[$file->getAttribute()][(string) $file] = $file;
-		}
+        /** @var Entities\IFile $file */
+        foreach ($this->files as $file) {
+            $filesByMedia[$file->getAttribute()][(string) $file] = $file;
+        }
 
-		foreach ($filesByMedia as $media => $files) {
-			// Check if we should join all files into one
-			if ($this->asset->getJoinFiles()) {
-				// Compile files collection
-				$result = $this->compiler->generate($files, $this->contentType);
+        foreach ($filesByMedia as $media => $files) {
+            // Check if we should join all files into one
+            if ($this->asset->getJoinFiles()) {
+                // Compile files collection
+                $result = $this->compiler->generate($files, $this->contentType);
 
-				echo $this->getElement($this->getPresenter()->link(':IPub:AssetsLoader:assets', ['type' => 'css', 'id' => $result->hash, 'timestamp' => $result->lastModified]), $media), PHP_EOL;
+                echo $this->getElement($this->getPresenter()->link(':IPub:AssetsLoader:assets', ['type' => 'css', 'id' => $result->hash, 'timestamp' => $result->lastModified]), $media), PHP_EOL;
 
-				// Leave files splitted
-			} else {
-				foreach ($files as $file) {
-					// Compile single file
-					$result = $this->compiler->generate([$file], $this->contentType);
+                // Leave files splitted
+            } else {
+                foreach ($files as $file) {
+                    // Compile single file
+                    $result = $this->compiler->generate([$file], $this->contentType);
 
-					echo $this->getElement($this->getPresenter()->link(':IPub:AssetsLoader:assets', ['type' => 'css', 'id' => $result->hash, 'timestamp' => $result->lastModified])), PHP_EOL;
-				}
-			}
-		}
-	}
+                    echo $this->getElement($this->getPresenter()->link(':IPub:AssetsLoader:assets', ['type' => 'css', 'id' => $result->hash, 'timestamp' => $result->lastModified])), PHP_EOL;
+                }
+            }
+        }
+    }
 
-	/**
-	 * Generates link
-	 *
-	 * @return string
-	 *
-	 * @throws Exceptions\InvalidStateException
-	 *
-	 * @throws Exceptions\InvalidStateException
-	 * @throws Nette\Application\UI\InvalidLinkException
-	 */
-	public function getLink() : string
-	{
-		$hasArgs = func_num_args() > 0;
+    /**
+     * Generates link
+     *
+     * @return string
+     *
+     * @throws Exceptions\InvalidStateException
+     *
+     * @throws Exceptions\InvalidStateException
+     * @throws Nette\Application\UI\InvalidLinkException
+     */
+    public function getLink() : string
+    {
+        $hasArgs = func_num_args() > 0;
 
-		$backup = NULL;
+        $backup = null;
 
-		if ($hasArgs) {
-			// Backup files
-			$backup = $this->files;
-			// Clear files collection
-			$this->clear();
+        if ($hasArgs) {
+            // Backup files
+            $backup = $this->files;
+            // Clear files collection
+            $this->clear();
 
-			// Get all arguments which could be files
-			$args = func_get_args();
-			$args = reset($args);
+            // Get all arguments which could be files
+            $args = func_get_args();
+            $args = reset($args);
 
-			// Create new collection from arguments
-			$newFiles = new Files\FilesCollection;
-			$newFiles->addFiles($args);
+            // Create new collection from arguments
+            $newFiles = new Files\FilesCollection;
+            $newFiles->addFiles($args);
 
-			// Create new files collection
-			$this->setFiles($newFiles);
-		}
+            // Create new files collection
+            $this->setFiles($newFiles);
+        }
 
-		$filesByMedia = [];
+        $filesByMedia = [];
 
-		/** @var Entities\IFile $file */
-		foreach ($this->files as $file) {
-			$filesByMedia[$file->getAttribute()][(string) $file] = $file;
-		}
+        /** @var Entities\IFile $file */
+        foreach ($this->files as $file) {
+            $filesByMedia[$file->getAttribute()][(string) $file] = $file;
+        }
 
-		if (count($filesByMedia) > 1) {
-			throw new Exceptions\InvalidStateException("Can't generate link for combined media.");
-		}
+        if (count($filesByMedia) > 1) {
+            throw new Exceptions\InvalidStateException("Can't generate link for combined media.");
+        }
 
-		if (!$this->asset->getJoinFiles()) {
-			throw new Exceptions\InvalidStateException("Can't generate link with disabled joinFiles.");
-		}
+        if (!$this->asset->getJoinFiles()) {
+            throw new Exceptions\InvalidStateException("Can't generate link with disabled joinFiles.");
+        }
 
-		// Compile files collection
-		$result = $this->compiler->generate(reset($filesByMedia), $this->contentType);
+        // Compile files collection
+        $result = $this->compiler->generate(reset($filesByMedia), $this->contentType);
 
-		$link = $this->getPresenter()->link(':IPub:AssetsLoader:assets', ['type' => 'css', 'id' => $result->hash, 'timestamp' => $result->lastModified]);
+        $link = $this->getPresenter()->link(':IPub:AssetsLoader:assets', ['type' => 'css', 'id' => $result->hash, 'timestamp' => $result->lastModified]);
 
-		if ($hasArgs && $backup !== NULL) {
-			$this->setFiles($backup);
-		}
+        if ($hasArgs && $backup !== null) {
+            $this->setFiles($backup);
+        }
 
-		return $link;
-	}
+        return $link;
+    }
 }
--- src/IPub/AssetsLoader/Filters/Files/CssUrlsFilter.php
+++ src/IPub/AssetsLoader/Filters/Files/CssUrlsFilter.php
@@ -26,122 +26,122 @@ use IPub\AssetsLoader\Files;
 
 class CssUrlsFilter extends FilesFilter
 {
-	/**
-	 * @var Caching\FileCache
-	 */
-	private $cache;
+    /**
+     * @var Caching\FileCache
+     */
+    private $cache;
 
-	/**
-	 * @var Application\Application
-	 */
-	private $application;
+    /**
+     * @var Application\Application
+     */
+    private $application;
 
-	/**
-	 * Filter constructor
-	 *
-	 * @param Caching\FileCache $cache
-	 * @param Application\Application $application
-	 */
-	public function __construct(Caching\FileCache $cache, Application\Application $application)
-	{
-		$this->cache = $cache;
-		$this->application = $application;
-	}
+    /**
+     * Filter constructor
+     *
+     * @param Caching\FileCache $cache
+     * @param Application\Application $application
+     */
+    public function __construct(Caching\FileCache $cache, Application\Application $application)
+    {
+        $this->cache = $cache;
+        $this->application = $application;
+    }
 
-	/**
-	 * Invoke filter
-	 *
-	 * @param string $code
-	 * @param Compilers\Compiler $compiler
-	 * @param string $file
-	 *
-	 * @return string
-	 */
-	public function __invoke(string $code, Compilers\Compiler $compiler, string $file) : string
-	{
-		$self = $this;
+    /**
+     * Invoke filter
+     *
+     * @param string $code
+     * @param Compilers\Compiler $compiler
+     * @param string $file
+     *
+     * @return string
+     */
+    public function __invoke(string $code, Compilers\Compiler $compiler, string $file) : string
+    {
+        $self = $this;
 
-		return preg_replace_callback('/url\([\'"]?(?![a-z]+:|\/+)([^\'")]+)[\'"]?\)/i', function ($matches) use ($self, $file) {
-			return "url('" . $self->absolutizeUrl($matches[1], $file) . "')";
-		}, $code);
-	}
+        return preg_replace_callback('/url\([\'"]?(?![a-z]+:|\/+)([^\'")]+)[\'"]?\)/i', function ($matches) use ($self, $file) {
+            return "url('" . $self->absolutizeUrl($matches[1], $file) . "')";
+        }, $code);
+    }
 
-	/**
-	 * Make relative url absolute
-	 *
-	 * @param string $url     image url
-	 * @param string $cssFile absolute css file path
-	 *
-	 * @return string
-	 */
-	public function absolutizeUrl(string $url, string $cssFile) : string
-	{
-		// Is already absolute
-		if (preg_match('/^([a-z]+:\/)?\//', $url)) {
-			return $url;
-		}
+    /**
+     * Make relative url absolute
+     *
+     * @param string $url     image url
+     * @param string $cssFile absolute css file path
+     *
+     * @return string
+     */
+    public function absolutizeUrl(string $url, string $cssFile) : string
+    {
+        // Is already absolute
+        if (preg_match('/^([a-z]+:\/)?\//', $url)) {
+            return $url;
+        }
 
-		// Get css file real path
-		$cssFile = Files\Path::normalize($cssFile);
+        // Get css file real path
+        $cssFile = Files\Path::normalize($cssFile);
 
-		// Remove query string
-		$url = preg_replace('/\?.*/', '', $url);
+        // Remove query string
+        $url = preg_replace('/\?.*/', '', $url);
 
-		// Create full file path
-		$filePath = realpath(rtrim(dirname(realpath($cssFile)), '/') . '/' . $url);
+        // Create full file path
+        $filePath = realpath(rtrim(dirname(realpath($cssFile)?:''), '/') . '/' . $url);
 
-		// Check if file exists
-		if (!$filePath || !file_exists($filePath)) {
-			return $url;
-		}
+        // Check if file exists
+        if (!$filePath || !file_exists($filePath)) {
+            return $url;
+        }
 
-		// Check for images which can be encoded
-		if (preg_match('/\.(gif|png|jpg)$/i', $url) && filesize($filePath) <= 10240 && preg_match('/\.(gif|png|jpg)$/i', $filePath, $extension)) {
-			$path = sprintf('data:image/%s;base64,%s', str_replace('jpg', 'jpeg', strtolower($extension[1])), base64_encode(file_get_contents($filePath)));
+        // Check for images which can be encoded
+        if (preg_match('/\.(gif|png|jpg)$/i', $url) && filesize($filePath) <= 10240 && preg_match('/\.(gif|png|jpg)$/i', $filePath, $extension)) {
+            $path = sprintf('data:image/%s;base64,%s', str_replace('jpg', 'jpeg', strtolower($extension[1])), base64_encode(file_get_contents($filePath)));
 
-			// Other files
-		} else {
-			// Create file hash
-			$fileHash = $this->getHash($filePath);
+            // Other files
+        } else {
+            // Create file hash
+            $fileHash = $this->getHash($filePath);
 
-			// Save file into cache
-			$this->cache->save(
-				$fileHash,
-				[
-					Caching\FileCache::CONTENT => $filePath
-				],
-				[
-					Caching\FileCache::TAGS  => ['ipub.assetsloader', 'ipub.assetsloader.images'],
-					Caching\FileCache::FILES => [$filePath]
-				]
-			);
+            // Save file into cache
+            $this->cache->save(
+                $fileHash,
+                [
+                    Caching\FileCache::CONTENT => $filePath
+                ],
+                [
+                    Caching\FileCache::TAGS  => ['ipub.assetsloader', 'ipub.assetsloader.images'],
+                    Caching\FileCache::FILES => [$filePath]
+                ]
+            );
 
-			// Create route for specific file
-			$presenter = $this->getPresenter();
+            // Create route for specific file
+            $presenter = $this->getPresenter();
 
-			$path = $presenter !== NULL ? $presenter->link(':IPub:AssetsLoader:files', ['id' => $fileHash]) : NULL;
-		}
+            $path = $presenter !== null ? $presenter->link(':IPub:AssetsLoader:files', ['id' => $fileHash]) : null;
+        }
 
-		return $path;
-	}
+        return $path;
+    }
 
-	/**
-	 * @return Application\IPresenter|NULL
-	 */
-	private function getPresenter() : ?Application\IPresenter
-	{
-		return $this->application->getPresenter();
-	}
+    /**
+     * @return Application\IPresenter|NULL
+     */
+    private function getPresenter() : ?Application\IPresenter
+    {
+        return $this->application->getPresenter();
+    }
 
-	/**
-	 * @param string $file
-	 *
-	 * @return string
-	 */
-	private function getHash(string $file) : string
-	{
-		$tmp = $file . filesize($file);
+    /**
+     * @param string $file
+     *
+     * @return string
+     */
+    private function getHash(string $file) : string
+    {
+        $tmp = $file . filesize($file);
 
-		return substr(md5($tmp), 0, 12);
-	}
+        return substr(md5($tmp), 0, 12);
+    }
 }
