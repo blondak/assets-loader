7fd9fef054735774f42c9ca5915de2680108ff61
Blonďák <blondak@neser.cz>
2025-12-19 09:02:10 +0100

    Update MimeMapper.php

--- src/IPub/AssetsLoader/Files/MimeMapper.php
+++ src/IPub/AssetsLoader/Files/MimeMapper.php
@@ -975,6 +975,7 @@ class MimeMapper
             'wmx'         => 'video/x-ms-wmx',
             'wmz'         => 'application/x-ms-wmz',
             'woff'        => 'application/x-font-woff',
+            'woff2'       => 'font/woff2',
             'wpd'         => 'application/vnd.wordperfect',
             'wpl'         => 'application/vnd.ms-wpl',
             'wps'         => 'application/vnd.ms-works',
